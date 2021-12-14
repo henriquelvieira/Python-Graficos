d656c54e798c8e91e6c44085551f90948257d86d
Henrique Lopes <henrique.l.vieira92@gmail.com>
2021-12-14 16:22:10 -0300

    Add files via upload

--- funcoesGraficos.py
+++ funcoesGraficos.py
@@ -35,6 +35,31 @@ def geraGraficoLinhas(listX=[],
     plt.plot(listX, listY) #LISTAS COM OS DADOS P/ OS EIXOS X E Y DO GRÁFICO
     plt.show() #PLOT DO GRÁFICO
 
+def geraGraficoPizza(listValues, listLabels, title=None):
+    '''
+    Função para Geração de Gráfico de Linhas
+
+    Args:
+        listValues []: Lista com os valores
+        listLabels []: Lista com as labels
+        title (str): Título
+
+    Return:
+        Gráfico gerado através do matplotlib
+    '''    
+    fig1, ax1 = plt.subplots()
+    ax1.pie(listValues, 
+            labels=listLabels, 
+            autopct='%1.1f%%',
+            shadow=True, 
+            startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    
+    if title:
+        plt.title(title)
+        
+    plt.show() #PLOT DO GRÁFICO
+
 if __name__ == '__main__':
     listX = ['2019', '2020', '2021']
     listY = [100, 250, 360]
@@ -45,3 +70,5 @@ if __name__ == '__main__':
                       labelX='Label Eixo X',
                       title='Título do Gráfico'
                       )    
+
+    geraGraficoPizza(listY, listX)      
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from funcoesGraficos import geraGraficoLinhas
+from funcoesGraficos import geraGraficoLinhas, geraGraficoPizza
 import pandas as pd
 
 vCaminhoTabela = r"dados.xlsx"
-vDados = pd.read_excel(vCaminhoTabela)
+dados = pd.read_excel(vCaminhoTabela)
 
-vListX = vDados['DATE'].tolist() #CONVERÇÃO DA COLUNA EM LISTA
-vListY = vDados['QTDE'].tolist() #CONVERÇÃO DA COLUNA EM LISTA
-vTotal = vDados['QTDE'].sum()
+listX = dados['DATE'].tolist() #CONVERÇÃO DA COLUNA EM LISTA
+listY = dados['QTDE'].tolist() #CONVERÇÃO DA COLUNA EM LISTA
+vTotal = dados['QTDE'].sum()
 
-geraGraficoLinhas(listX=vListX, 
-                  listY=vListY, 
+geraGraficoLinhas(listX=listX, 
+                  listY=listY, 
                   labelY='Qtde',
                   title=f'Acompanhamento - Total {vTotal}'
-                 )    
+                 )   
+
+geraGraficoPizza(listY, listX)    
