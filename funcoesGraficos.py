e7dbef9661506b434824500ed02f5eea1e57deff
Henrique Lopes <henrique.l.vieira92@gmail.com>
2021-12-15 11:17:54 -0300

    Add files via upload

--- funcoesGraficos.py
+++ funcoesGraficos.py
@@ -2,8 +2,11 @@ from matplotlib import pyplot as plt
 
 def geraGraficoLinhas(listX=[], 
                       listY=[], 
+                      listX2=None, 
+                      listY2=None, 
                       labelX=None, 
                       labelY=None, 
+                      legend=None,
                       title=None,
                       style='ggplot'):
     '''
@@ -11,16 +14,19 @@ def geraGraficoLinhas(listX=[],
 
     Args:
         listX []: Lista com os dados do eixo X
-        listY []: Lista com os dados do eixo Y 
+        listY []: Lista com os dados do eixo Y
+        listX2 []: Lista com os dados do eixo X (segunda série de dados)
+        listY2 []: Lista com os dados do eixo Y (segunda série de dados)
         labelX []: Label do eixo X
         labelY []: Label do eixo Y
+        legend []: Legenda do gráfico
         title (str): Título
         style (str): Stylo do Gráfico
 
     Return:
         Gráfico gerado através do matplotlib
-    '''                      
-    
+    '''  
+    plt.figure(figsize=(10, 6))
     plt.style.use(style) #DEFINIÇÃO DO STYLO DO GRÁFICO
     if title:
         plt.title(title) #DEFINIÇÃO DO TÍTULO
@@ -32,7 +38,14 @@ def geraGraficoLinhas(listX=[],
         plt.xlabel(labelX) #LABEL DO EIXO Y
 
     plt.xticks(rotation=25) #ROTAÇÃO DO LABEL DO EIXO X
-    plt.plot(listX, listY) #LISTAS COM OS DADOS P/ OS EIXOS X E Y DO GRÁFICO
+    if listX2 and listX2:
+        plt.plot(listX, listY, listX2, listY2) #LISTAS COM OS DADOS P/ OS EIXOS X E Y DO GRÁFICO
+    else:
+        plt.plot(listX, listY) #LISTAS COM OS DADOS P/ OS EIXOS X E Y DO GRÁFICO
+    
+    if legend:
+        plt.legend(legend)
+
     plt.show() #PLOT DO GRÁFICO
 
 def geraGraficoPizza(listValues, listLabels, title=None):
@@ -70,5 +83,15 @@ if __name__ == '__main__':
                       labelX='Label Eixo X',
                       title='Título do Gráfico'
                       )    
+
+    listY2 = [120, 60, 320]
+    geraGraficoLinhas(listX=listX, 
+                      listY=listY, 
+                      listX2=listX,
+                      listY2=listY2,
+                      labelY='Label Eixo Y',
+                      labelX='Label Eixo X',
+                      title='Título do Gráfico'
+                      )  
 
     geraGraficoPizza(listY, listX)      
